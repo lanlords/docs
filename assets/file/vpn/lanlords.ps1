3a65c989362d3efb647156d3a66a88d47b125937
Jona Koudijs <jona@koudijs.me>
2020-06-29 15:20:33 +0200

    Improve VPN configure script

--- assets/file/vpn/lanlords.ps1
+++ assets/file/vpn/lanlords.ps1
@@ -8,19 +8,19 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 
 
 # output status to console
-Write-Host "=> De Lanlords VPN connectie wordt opgezet .."
+Write-Output "=> De Lanlords VPN connectie wordt opgezet .."
 Start-Sleep -Seconds 1
 
 # Configure Lanlords VPN
 Add-VpnConnection -Name 'Lanlords VPN' -ServerAddress 'vpn.lanlords.nl' -TunnelType L2tp -AuthenticationMethod Chap -EncryptionLevel Optional -L2tpPsk 'lanlords' -SplitTunneling $True -RememberCredential -Confirm:$false -Force
 
 # output status to console
-Write-Host "=> De Lanlords routes worden ingesteld .."
+Write-Output "=> De Lanlords routes worden ingesteld .."
 Start-Sleep -Seconds 1
 
 # Force specific Lanlords routes
 Add-VpnConnectionRoute -ConnectionName "Lanlords VPN" -DestinationPrefix 192.168.1.0/24
 
 # output status to console
-Write-Host "=> De Lanlords VPN is geconfigureerd. Volg de rest van de stappen op docs.lanlords.nl"
+Write-Output "=> De Lanlords VPN is geconfigureerd. Volg de rest van de stappen op docs.lanlords.nl"
 Start-Sleep -Seconds 4
